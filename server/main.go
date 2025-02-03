271a3c747c08d1f87c187f42ad9ced4e7a755829
ErnestJasa <ernestjasaitis@gmail.com>
2025-02-03 12:25:27 +0200

    added environment port getter.

--- server/main.go
+++ server/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/youtubers", GetChannels)
 	mux.HandleFunc("/api/categories", GetCategories)
 
 	handlerWithCORS := EnableCORS(mux)
-	log.Println("server is running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", handlerWithCORS))
+	log.Println("server is running on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, handlerWithCORS))
 }
